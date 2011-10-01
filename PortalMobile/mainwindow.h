70ba396e5c05e29ada628f51a1c5940f1e0e132b
laabroo <oxlet_wtn@yahoo.com>
2011-10-01 21:43:20 +0700

    start project

---
+++ PortalMobile/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui/QMainWindow>
+#include <QtWebKit>
+#include <QWebView>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    enum ScreenOrientation {
+        ScreenOrientationLockPortrait,
+        ScreenOrientationLockLandscape,
+        ScreenOrientationAuto
+    };
+
+    explicit MainWindow(QWidget *parent = 0);
+    virtual ~MainWindow();
+
+    // Note that this will only have an effect on Symbian and Fremantle.
+    void setOrientation(ScreenOrientation orientation);
+
+    void showExpanded();
+
+private:
+    Ui::MainWindow *ui;
+    QWebView *myWeb;
+
+private :
+    loadWeb();
+};
+
+#endif // MAINWINDOW_H
